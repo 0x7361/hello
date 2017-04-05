2c183b60a4071017e2839ca32ef1519cc69eb921
Aleksei 0x7361 <0x7361@gmail.com>
2017-04-05 04:20:59 +0300

    reverse

--- hello.go
+++ hello.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/0x7361/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world!\n")
+	fmt.Printf(stringutil.Reverse("\nПривет, мир!"))
 }
-
